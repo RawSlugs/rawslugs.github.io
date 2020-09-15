81eb85bb2de10ee12207b350cb4b3bd90b609f55
RawSlugs <yglhgs@gmail.com>
2020-09-15 15:34:49 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function OnSubmit()
 {
   let lerr = document.querySelector("#lerr")
-  lerr.InnerText = "Error: Account Not Found!"
+  lerr.InnerHTML = "Error: Account Not Found!"
   lerr.classList.remove("d-none")
 }
